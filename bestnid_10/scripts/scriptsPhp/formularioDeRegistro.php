54c93a495e7e06e8af6a0d5da2e2b55913ef7973
miguelguerra6614 <miguelguerra@hotmail.com.ar>
2015-06-25 23:28:34 -0300

    cambios

--- bestnid_10/scripts/scriptsPhp/formularioDeRegistro.php
+++ bestnid_10/scripts/scriptsPhp/formularioDeRegistro.php
@@ -14,16 +14,15 @@
 	$contraseniaR=$_REQUEST['contraseniaR'];
 	$tipoDeUsuario= "usuario";
 	$estado= "activo";
-	$valida= "1";
 
 	abrirConexion();
-	$query="SELECT `email`
-			FROM `bestnid`.`usuario`
-			WHERE email = '$email'";
-	$result = mysqli_query($GLOBALS['connection'],$query);
-	// $result= queryUsuarioEmailEmail($email);
-	
-	//queryTodas($columnas, $tabla, $condWhereAdicionales)
+	// $query="SELECT `email`
+	// 		FROM `bestnid`.`usuario`
+	// 		WHERE email = '$email'";
+	// 	$result = mysqli_query($GLOBALS['connection'],$query);
+	//$result= queryUsuarioEmailEmail($email);
+
+	$result = queryUsuarioEmailEmail($email);
 	if(mysqli_num_rows($result) > 0){
 		echo 0;
 
@@ -31,9 +30,9 @@
 	else{
 		
 		$query="INSERT INTO `bestnid`.`usuario` (`idUsuario`, `email`, 
-		     `nombre`, `apellido`, `telFijo`, `telMovil`, `contrasenia`, `tipoDeUsuario`, `estado`, `valida`)
+		     `nombre`, `apellido`, `telFijo`, `telMovil`, `contrasenia`, `tipoDeUsuario`, `estado`)
 		   	 VALUES(NULL,'$email','$nombre','$apellido','$telFijo','$telMovil','$contrasenia','$tipoDeUsuario',
-		   	 	'$estado', '$valida')";
+		   	 	'$estado')";
 		mysqli_query($GLOBALS['connection'],$query);
 		echo 1;
 
